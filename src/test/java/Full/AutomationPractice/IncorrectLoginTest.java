a91561c8b28100a271fc439aa53bb70e2f4e2513
deepthi <deepthishn@gmail.com>
2022-05-22 16:10:16 +0300

    22nd May Test2

---
+++ src/test/java/Full/AutomationPractice/IncorrectLoginTest.java
@@ -0,0 +1,29 @@
+package Full.AutomationPractice;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+import resources.Base;
+
+public class IncorrectLoginTest extends Base{
+	
+	public static Logger log=LogManager.getLogger(Base.class.getName());
+	public WebDriver driver;
+	
+	@BeforeTest
+	public void getBrowser() throws IOException
+	{
+		//initializeDriver from Base returns a driver
+		driver=initializeDriver();
+		log.info("Driver is initialized");
+		//Now load & read data.properties file to get URL
+		prop.load(fis);
+		driver.get(prop.getProperty("url"));
+		log.info("Navigated to Automation Practice page");
+	}
+
+}
